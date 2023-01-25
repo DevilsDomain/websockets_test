ea95859dee6d868d6f2454eacea6863201a7b811
DevilsDomain <alaraerzincan@gmail.com>
2023-01-25 11:16:55 +0100

    .

--- modules/socket.js
+++ modules/socket.js
@@ -4,11 +4,13 @@ import { defineNuxtModule } from "@nuxt/kit";
 const userMap = {
   // map socket.id to user nick
 };
+
 function buildMessage(who, what) {
   // convert to POJO (Plain Old Javascript Object)
   // information sent thru the socket has to be able to be stringified & parsed
   // (JSON.stringify, JSON.parse)
-  return { id: "1", message: what };
+  return { id: "1", message: who.nickname + ': ' + what };
+
 }
 
 export default defineNuxtModule({
@@ -19,17 +21,22 @@ export default defineNuxtModule({
 
       nuxt.hook("close", () => io.close());
 
-      io.on("connection", (socket) => {
-        console.log("Connection", socket.id);
+      io.on('connection', (socket) => {
+        console.log('Connection', socket.id);
+        socket.on('join', (data) => {
+          socket.nickname = data.nickname;
+          socket.emit('message', buildMessage(socket, `welcome ${socket.nickname}`));
+          socket.broadcast.emit('message', buildMessage(socket, `${socket.nickname} joined`));
+        });
       });
 
       io.on("connect", (socket) => {
-        socket.emit("message", buildMessage(socket, `welcome ${socket.id}`));
-
-        socket.broadcast.emit(
-          "message",
-          buildMessage(socket, `${socket.id} joined`)
-        );
+        socket.emit('message', buildMessage(socket, `welcome ${socket.nickname}`));
+        socket.broadcast.emit('message', buildMessage(socket, `${socket.nickname} joined`));
+        // socket.broadcast.emit(
+        //   "message",
+        //   buildMessage(socket, `${socket.id} joined`)
+        // );
 
         socket.on("message", function message(data) {
           console.log("message received: %s", data);
@@ -38,10 +45,7 @@ export default defineNuxtModule({
 
         socket.on("disconnecting", () => {
           console.log("disconnected", socket.id);
-          socket.broadcast.emit(
-            "message",
-            buildMessage(socket, `${socket.id} left`)
-          );
+          socket.broadcast.emit('message', buildMessage(socket, `${socket.nickname} left`));
         });
       });
     });
